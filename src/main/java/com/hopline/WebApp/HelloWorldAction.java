e80a2ffe0405ffcdeacc8fc21bce20571b31d8d1
Akshansh986 <akshanshsingh986@gmail.com>
2016-09-28 11:16:14 +0530

    dfg

--- src/main/java/com/hopline/WebApp/HelloWorldAction.java
+++ src/main/java/com/hopline/WebApp/HelloWorldAction.java
@@ -1,9 +1,17 @@
 package com.hopline.WebApp;
 
+import java.util.List;
+
+import com.hopline.WebApp.model.dao.Category;
+import com.hopline.WebApp.rest.framework.ServiceLocator;
+
 public class HelloWorldAction {
 	private String name;
 
 public String execute() throws Exception {
+	List<Category> categories = ServiceLocator.getInstance()
+			.getService(CategoryServiceImpl.class).retrieveAllCategory();
+
    return "success";
 }
 
