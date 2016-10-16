0775f017a6721fa33f31e7997cac89f0e08d1305
Akshansh986 <akshanshsingh986@gmail.com>
2016-10-16 14:13:19 +0530

    Vendor side project setup

---
+++ src/main/java/com/hoplineVendorServices/model/AddOnTo.java
@@ -0,0 +1,46 @@
+package com.hoplineVendorServices.model;
+
+public class AddOnTo implements java.io.Serializable {
+
+	private Integer idaddOn;
+	private String name;
+	private Double price;
+	private String stockYn;
+
+	
+	//For UI frontend
+	private boolean selected;
+	
+	public Integer getIdaddOn() {
+		return idaddOn;
+	}
+	public void setIdaddOn(Integer idaddOn) {
+		this.idaddOn = idaddOn;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public String getStockYn() {
+		return stockYn;
+	}
+	public void setStockYn(String stockYn) {
+		this.stockYn = stockYn;
+	}
+	public boolean isSelected() {
+		return selected;
+	}
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+
+	
+}
---
+++ src/main/java/com/hoplineVendorServices/model/ShopTo.java
@@ -0,0 +1,41 @@
+package com.hoplineVendorServices.model;
+
+public class ShopTo implements java.io.Serializable {
+
+	private Integer idshop;
+	private String shopName;
+	private String phone;
+	private String imgUrl;
+	private String activeYn;
+	public Integer getIdshop() {
+		return idshop;
+	}
+	public void setIdshop(Integer idshop) {
+		this.idshop = idshop;
+	}
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	public String getActiveYn() {
+		return activeYn;
+	}
+	public void setActiveYn(String activeYn) {
+		this.activeYn = activeYn;
+	}
+	
+}
