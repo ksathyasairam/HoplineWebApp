71d1f34ee886466ec9c99c95b66af36f797a009a
Ayush Arnav <ayush.arnav@gmail.com>
2017-03-15 01:09:31 +0530

    vendor notification

--- src/main/webapp/webvendor/app/mainApp.js
+++ src/main/webapp/webvendor/app/mainApp.js
@@ -1,13 +1,33 @@
-var mainApp = angular.module('mainApp', [ 'ngRoute' ]);
+var mainApp = angular.module('mainApp', [ 'angular-web-notification' ]);
 
 mainApp.controller('MainCtrl', [
 		'$scope',
 		'$http',
 		'$timeout',
-		function($scope, $http, $timeout) {
+		'webNotification',
+		function($scope, $http, $timeout, webNotification) {
 
 			var timer;
-			$scope.allOrders={};
+			$scope.allOrders = null;
+			
+			$scope.notify=function(){
+			
+			webNotification.showNotification('Hopline Notification', {
+				  body: 'You have new orders!!',
+				  icon: 'img/hoplogo.png',
+				  onClick: function onNotificationClicked() {
+				    console.log('Notification clicked.');
+				  },
+				  //autoClose: 4000 //auto close the notification after 4 seconds (you can manually close it via hide function)
+				}, function onShow(error, hide) {
+				  if (error) {
+				    window.alert('Unable to show notification: ' + error.message);
+				  } else {
+				    console.log('Notification Shown.');
+				  }
+				});
+			
+			}
 
 			function fetchAllOrders() {
 				// When the timeout is defined, it returns a
@@ -17,7 +37,7 @@ mainApp.controller('MainCtrl', [
 						"orderStates" : [ "BIG_ORDER_PAY", "BIG_ORDER_CALL",
 								"DEFAULTER_CALL", "OK_ORDER", "PREPARING",
 								"READY_FOR_PICKUP" ],
-						"shopId" : 6,
+						"shopId" : localStorage.getItem('shopId'),
 						"success" : false
 					}
 				};
@@ -26,18 +46,38 @@ mainApp.controller('MainCtrl', [
 						'Content-Type' : 'application/json;'
 					}
 				};
-
 				timer = $timeout(function() {
-					//console.log("Timeout executed", Date.now());
+					// console.log("Timeout executed", Date.now());
 				}, 3000);
 
 				timer.then(function() {
-					//console.log("Timer resolved!");
+					// console.log("Timer resolved!");
 
-					$http.post("/rest/fetchOrders", data, config).then(
+					$http.post("/rest/fetchOrdersWeb", data, config).then(
 							function(response) {
-								console.log(response);
-								$scope.allOrders=response
+								//console.log(response);
+								var count=0;
+								if($scope.allOrders!=null){
+								for(var i=0;i<response.data.orders.length;i++){
+									for(var j=0;j<$scope.allOrders.data.orders.length;j++){
+										if(response.data.orders[i].idorder==$scope.allOrders.data.orders[j].idorder){
+											count=count+1;
+										}
+									}									
+								}
+								if(count<response.data.orders.length){
+									$scope.notify();
+									
+								}
+								else if(count==response.data.orders.length){
+									console.log(count);
+								}
+								else if(count>response.data.orders.length){
+									console.log(count);
+								}
+								count=0;
+								}
+								$scope.allOrders = response
 								fetchAllOrders();
 							}, function(response) {
 								console.log("failed response");
@@ -46,8 +86,8 @@ mainApp.controller('MainCtrl', [
 				});
 			}
 			fetchAllOrders();
-			
-			$scope.fetchAllOrdersWithoutInterval=function() {
+
+			$scope.fetchAllOrdersWithoutInterval = function() {
 				// When the timeout is defined, it returns a
 				// promise object.
 				var data = {
@@ -55,7 +95,7 @@ mainApp.controller('MainCtrl', [
 						"orderStates" : [ "BIG_ORDER_PAY", "BIG_ORDER_CALL",
 								"DEFAULTER_CALL", "OK_ORDER", "PREPARING",
 								"READY_FOR_PICKUP" ],
-						"shopId" : 6,
+						"shopId" : localStorage.getItem('shopId'),
 						"success" : false
 					}
 				};
@@ -64,12 +104,12 @@ mainApp.controller('MainCtrl', [
 						'Content-Type' : 'application/json;'
 					}
 				};
-					$http.post("/rest/fetchOrders", data, config).then(
-							function(response) {
-								$scope.allOrders=response;
-							}, function(response) {
-								console.log("failed response");
-							})
+				$http.post("/rest/fetchOrders", data, config).then(
+						function(response) {
+							$scope.allOrders = response;
+						}, function(response) {
+							console.log("failed response");
+						})
 			}
 
 			$scope.cancelOrder = function(orderId) {
@@ -91,7 +131,7 @@ mainApp.controller('MainCtrl', [
 
 				$http.post("/rest/udpateOrderStatus", data, config).then(
 						function(response) {
-							//console.log(response);
+							// console.log(response);
 							$scope.fetchAllOrdersWithoutInterval();
 						}, function(response) {
 							console.log("failed to cancel");
@@ -117,7 +157,7 @@ mainApp.controller('MainCtrl', [
 
 				$http.post("/rest/udpateOrderStatus", data, config).then(
 						function(response) {
-							//console.log(response);
+							// console.log(response);
 							$scope.fetchAllOrdersWithoutInterval();
 						}, function(response) {
 							console.log("failed to cancel");
@@ -142,7 +182,7 @@ mainApp.controller('MainCtrl', [
 
 				$http.post("/rest/udpateOrderStatus", data, config).then(
 						function(response) {
-							//console.log(response);
+							// console.log(response);
 							$scope.fetchAllOrdersWithoutInterval();
 						}, function(response) {
 							console.log("failed to cancel");
@@ -168,7 +208,7 @@ mainApp.controller('MainCtrl', [
 
 				$http.post("/rest/udpateOrderStatus", data, config).then(
 						function(response) {
-							//console.log(response);
+							// console.log(response);
 							$scope.fetchAllOrdersWithoutInterval();
 						}, function(response) {
 							console.log("failed to cancel");
@@ -193,7 +233,7 @@ mainApp.controller('MainCtrl', [
 
 				$http.post("/rest/udpateOrderStatus", data, config).then(
 						function(response) {
-							//console.log(response);
+							// console.log(response);
 							$scope.fetchAllOrdersWithoutInterval();
 						}, function(response) {
 							console.log("failed to cancel");
@@ -218,8 +258,32 @@ mainApp.controller('MainCtrl', [
 
 				$http.post("/rest/udpateOrderStatus", data, config).then(
 						function(response) {
-							//console.log(response);
+							// console.log(response);
 							$scope.fetchAllOrdersWithoutInterval();
+						}, function(response) {
+							console.log("failed to cancel");
+						})
+			}
+
+			$scope.login = function(username, password) {
+				console.log(username, password)
+				var data = {
+					"shop" : {
+						"username" : username,
+						"password" : password
+					}
+				}
+				var config = {
+					headers : {
+						'Content-Type' : 'application/json;'
+					}
+				};
+
+				$http.post("/rest/vendorLogin", data, config).then(
+						function(response) {
+							//console.log(response);
+							localStorage.setItem('shopId',response.data.idshop);
+							window.location = '/webvendor/index.html';
 						}, function(response) {
 							console.log("failed to cancel");
 						})
